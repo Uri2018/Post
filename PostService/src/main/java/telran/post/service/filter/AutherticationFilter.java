be97548d210366d4bc9118d2a1104fbc39a3b445
URI <URI@LAPTOP-FN744CJD>
2018-10-17 20:57:46 +0300

    class2

--- PostService/src/main/java/telran/post/service/filter/AutherticationFilter.java
+++ PostService/src/main/java/telran/post/service/filter/AutherticationFilter.java
@@ -9,30 +9,60 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
+
+import telran.post.configoration.AccountConfigoration;
+import telran.post.configoration.AccountUserCredential;
+import telran.post.dau.UserAccountRepository;
+import telran.post.domain.UserAccount;
 @Service
 @WebFilter(urlPatterns= {"/forum/*"})
 @Order(1)
 public class AutherticationFilter implements Filter {
-
+@Autowired
+	AccountConfigoration userconfigoration;
+	@Autowired
+	UserAccountRepository userAccountRepository;
+	
+	
+	
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
+		
 
 	}
 
 	@Override
 	public void doFilter(ServletRequest regs, ServletResponse resp, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
-
+		HttpServletRequest request=(HttpServletRequest)regs;
+		HttpServletResponse response=(HttpServletResponse)resp;
+		String auth=request.getHeader("Authorization");
+	AccountUserCredential	usercredental=userconfigoration .tokenDecode(auth);
+       UserAccount userAccount=userAccountRepository.findById(usercredental.getLogin()).orElse(null);
+       if(userAccount==null)
+       {
+    	   response.sendError(401,"Unauthorized");
+       }
+       else
+       {
+    	   if(!userAccount.getPassword().equals(usercredental.getPassword()))
+    	   {
+    		   response.sendError(403,"Forbidden");
+    	   }
+       }
+       chain.doFilter(request, response);
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
+	
 
 	}
 
